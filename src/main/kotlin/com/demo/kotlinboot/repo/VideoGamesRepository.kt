39f0cb93a09e0c94e55155fe3320014b15664e35
asmi2 <asmi24886@gmail.com>
2021-01-17 22:40:56 +0530

    Changed code style

--- src/main/kotlin/com/demo/kotlinboot/repo/VideoGamesRepository.kt
+++ src/main/kotlin/com/demo/kotlinboot/repo/VideoGamesRepository.kt
@@ -5,5 +5,4 @@ import org.springframework.data.mongodb.repository.MongoRepository
 import org.springframework.stereotype.Repository
 
 @Repository
-interface VideoGamesRepository : MongoRepository<VideoGames, String> {
-}
+interface VideoGamesRepository : MongoRepository<VideoGames, String> {}
